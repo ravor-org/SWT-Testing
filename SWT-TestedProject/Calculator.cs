82db81596f90375ee2b24453e47b230e838baa6c
Steffen Krueger <steffen.krueger@damovo.com>
2024-06-24 14:14:30 +0200

    Calculator recreated after gone missing

--- SWT-TestedProject/Calculator.cs
+++ SWT-TestedProject/Calculator.cs
@@ -1,1 +1,42 @@
-﻿// ... where did it go?
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_TestedProject
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double MultiplyDouble(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public int RemainingDiv(int a, int b) 
+        {
+            return a % b;
+        }
+    }
+}
